6fab38347c4b67cc1f97b89206d0756ac9878d8e
SaMuEl13GiNeR <sam.gi2002@gmail.com>
2023-01-30 10:08:26 +0100

    primer commit

---
+++ src/test/java/com/sim/mvc/SimarropopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimarropopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
