465ef3e3a32f3da39c94cd8306724a086fb3f386
SaMuEl13GiNeR <sam.gi2002@gmail.com>
2023-02-02 10:34:16 +0100

    Controller Articulo complete

--- src/main/java/com/sim/mvc/controllers/ArticuloRestController.java
+++ src/main/java/com/sim/mvc/controllers/ArticuloRestController.java
@@ -35,8 +35,30 @@ public class ArticuloRestController {
 	private IArticuloService articuloService;
 
 	@GetMapping("/articulos")
-	public List<Articulo> index() {
+	public List<Articulo> findAll() {
 		return articuloService.findAll();
+	}
+	
+	@GetMapping("/articulo/{id}")
+	public ResponseEntity<?> findById(@PathVariable Long id) {
+		
+		Articulo articulo = null;
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			articulo = articuloService.findById(id);
+		} catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(articulo == null) {
+			response.put("mensaje", "El articulo ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Articulo>(articulo, HttpStatus.OK);
 	}
 	
 	@PostMapping("/articulo/nuevo")
@@ -67,6 +89,119 @@ public class ArticuloRestController {
 		response.put("mensaje", "El articulo ha sido creado con éxito!");
 		response.put("articulo", articuloNew);
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@DeleteMapping("/articulo/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id) {
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			articuloService.delete(id);
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al eliminar el articulo de la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "Articulo eliminado con éxito!");
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
+	}
+	
+	
+	@PutMapping("/articulo/{id}")
+	public ResponseEntity<?> update(@RequestBody Articulo articulo, BindingResult result, @PathVariable Long id) {
+
+		Articulo articuloActual = articuloService.findById(id);
+
+		Articulo articuloUpdated = null;
+
+		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '" + err.getField() +"' "+ err.getDefaultMessage())
+					.collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		
+		if (articuloActual == null) {
+			response.put("mensaje", "Error: no se pudo editar, el articulo ID: "
+					.concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+
+		try {
+
+			articuloActual.setTitulo(articulo.getTitulo());
+			articuloActual.setLikes(articulo.getLikes());
+			articuloActual.setDescripcion(articulo.getDescripcion());
+			articuloActual.setPrecio(articulo.getPrecio());
+			articuloActual.setEstado(articulo.getEstado());
+			articuloActual.setUsuario(articulo.getUsuario());
+			articuloActual.setCategoria(articulo.getCategoria());
+			articuloActual.setFotos(articulo.getFotos());
+
+
+			articuloUpdated = articuloService.save(articuloActual);
+
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al actualizar el articulo en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		response.put("mensaje", "El articulo ha sido actualizado con éxito!");
+		response.put("articulo", articuloUpdated);
+
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	@GetMapping("/articulos/usuario/{id}")
+	public List<Articulo> findByUsuarioId(@PathVariable Long id){
+		return articuloService.findByUsuarioId(id);
+	}
+
+	@GetMapping("/articulos/categoria/{id}")
+	public List<Articulo> findByCategoriaId(@PathVariable Long id){
+		return articuloService.findByCategoriaId(id);
+	}
+	
+	@GetMapping("/articulos/precio/lower")
+	public List<Articulo> findAllOrderByLowerPrecio(){
+		return articuloService.findAllOrderByLowerPrecio();
+	}
+	
+	@GetMapping("/articulos/precio/higher")
+	public List<Articulo> findAllOrderByHigherPrecio(){
+		return articuloService.findAllOrderByHigherPrecio();
+	}
+	
+	@GetMapping("/articulo/{titulo}")
+	public ResponseEntity<?> findByTitulo(@PathVariable String titulo) {
+		
+		Articulo articulo = null;
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			articulo = articuloService.findByTitulo(titulo);
+		} catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar la consulta en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		if(articulo == null) {
+			response.put("mensaje", "El articulo ID: ".concat(titulo.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Articulo>(articulo, HttpStatus.OK);
 	}
 	
 }
